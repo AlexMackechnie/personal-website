1f299312272c047a6a3c208233b54bd63583289a
Alex Mackechnie <alexmackechnie@Alexs-MacBook-Pro.local>
2020-12-06 20:21:49 +0000

    Open Graph updates;

--- personal-website-ui/src/components/Post.js
+++ personal-website-ui/src/components/Post.js
@@ -3,13 +3,18 @@ import Header from './Header';
 import './Post.css';
 import { Helmet } from "react-helmet";
 
-function Post({children, toggleSidebar, seoTitle, seoDescription}) {
+function Post({children, toggleSidebar, seoTitle, seoDescription, seoOgTitle, seoOgType, seoOgDescription, seoOgImage}) {
     return (
         <>
             <Helmet>
                 <html lang="en" />
                 <title>{seoTitle}</title>
                 <meta name="description" content={seoDescription} />
+                <meta name="author" content="Alex Mackechnie" />
+                <meta property="og:title" content={seoOgTitle} />
+                <meta property="og:type" content={seoOgType} />
+                <meta property="og:description" content={seoOgDescription} />
+                <meta property="og:image" content={seoOgImage} />
             </Helmet>
             <Header title="blog" toggleSidebar={toggleSidebar} />
             <hr></hr>
